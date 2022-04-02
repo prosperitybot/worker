4f1c6174aacfdebcac08b0771acc576032826405
Ben Hall <ben@benh.codes>
2022-04-02 12:01:39 +0100

    updated worker
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/app/routes.go
+++ src/app/routes.go
@@ -10,6 +10,8 @@ func HandleRequests(router *mux.Router, testHandler TestHandler) {
 	r := router.PathPrefix("/").Subrouter()
 	testRouter := r.PathPrefix("/test").Subrouter()
 
+	r.HandleFunc("", BaseRequest).Methods(http.MethodPost)
+
 	router.HandleFunc("/health", CheckHealth).Methods(http.MethodGet)
 
 	testRouter.HandleFunc("", testHandler.Test).Methods(http.MethodGet)
