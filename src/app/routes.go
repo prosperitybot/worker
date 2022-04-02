356a101aae7ccecbc39ccb7d80beb020eea7b96f
Ben Hall <ben@benh.codes>
2022-04-02 15:00:47 +0100

    fix: Route not found
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/app/routes.go
+++ src/app/routes.go
@@ -10,7 +10,7 @@ func HandleRequests(router *mux.Router, testHandler TestHandler) {
 	r := router.PathPrefix("/").Subrouter()
 	testRouter := r.PathPrefix("/test").Subrouter()
 
-	r.HandleFunc("", BaseRequest).Methods(http.MethodPost)
+	r.HandleFunc("/t", BaseRequest).Methods(http.MethodPost)
 
 	router.HandleFunc("/health", CheckHealth).Methods(http.MethodGet)
 
