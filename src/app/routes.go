179bab713c7b3a9fd3c914433cfab237ac0e653a
Ben Hall <ben@benh.codes>
2022-04-03 14:09:21 +0100

    fix: Worker
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/app/routes.go
+++ src/app/routes.go
@@ -7,12 +7,9 @@ import (
 )
 
 func HandleRequests(router *mux.Router, testHandler TestHandler) {
-	r := router.PathPrefix("/").Subrouter()
-	testRouter := r.PathPrefix("/test").Subrouter()
+	// r := router.PathPrefix("/").Subrouter()
 
-	r.HandleFunc("/interactions", BaseRequest).Methods(http.MethodPost)
+	router.HandleFunc("/interactions", BaseRequest).Methods(http.MethodPost)
 
 	router.HandleFunc("/health", CheckHealth).Methods(http.MethodGet)
-
-	testRouter.HandleFunc("", testHandler.Test).Methods(http.MethodGet)
 }
