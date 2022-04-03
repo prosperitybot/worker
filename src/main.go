f64c07cf32f9d90992358144e06d6f145c4f84b5
Ben Hall <ben@benh.codes>
2022-04-03 14:03:23 +0100

    fix: Worker
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/main.go
+++ src/main.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/prosperitybot/worker/app"
+	"github.com/prosperitybot/worker/logging"
 )
 
 func main() {
 	fmt.Println("Loading environment variables")
 	_ = godotenv.Load()
 	fmt.Println("Configuring Logging system...")
+	_ = logging.Init()
 	app.Start()
 }
